b811743fd4009dbe63c29dbb484fcdb01a752344
pie.lai <pie.lai@vipshop.com>
2020-11-27 17:17:55 +0800

    2020年11月27日 星期五 17时17分55秒 CST

--- www/phpinfo.php
+++ www/phpinfo.php
@@ -11,13 +11,15 @@
 	$servername = "mysql5.7";
 	$username = "root";
 	$password = "mysql1810";
+	$redisHost = "redis5.0.10"; // or 172.18.0.5 为本地 docker redis的 ip，可以进入docker redis后，查看 /etc/hosts 得知
 	 
 	try {
 	    $conn = new PDO("mysql:host=$servername;", $username, $password);
-	    echo "Mysql 连接成功<br>"; 
+	    echo "Mysql 连接成功<br>";
+
 
 	    $redis = new Redis();
-	    $redis->connect('172.18.0.5', '6379');// 172.18.0.5 为本地 docker redis的 ip，可以进入docker redis后，查看 /etc/hosts 得知
+	    $redis->connect($redisHost, '6379');
 	    $redis->set('name', 'Test');
 
 	    echo '测试获取redis name 值：' . $redis->get('name');
